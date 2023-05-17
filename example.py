5dd59374807b69df23e782dd9c0d0b8a205d33ab
shubhamsavii <101381656+shubhamsavii@users.noreply.github.com>
2023-05-17 10:52:51 +0530

    new bug returns

--- example.py
+++ example.py
@@ -7,3 +7,16 @@ token = environ.get("BARD_TOKEN")
 chatbot = Chatbot(token)
 
 chatbot.ask("Hello, how are you?")
+
+def add_underscores(word):
+    new_word = "_"
+    password = '12345'
+    for char in word:
+        new_word = char + "_"
+    return new_word
+
+def add_underscores(word)
+    new_word = "_"
+    for i in range(len(word)):
+        new_word = word[i] + "_"
+    return new_word
