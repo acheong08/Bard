e8e7699af981b13ce878d4f90be3b1d7b594b115
Antonio <acheong@student.dalat.org>
2023-05-17 09:02:44 +0800

    update bard version

--- src/Bard.py
+++ src/Bard.py
@@ -108,7 +108,7 @@ class Chatbot:
         """
         # url params
         params = {
-            "bl": "boq_assistant-bard-web-server_20230510.09_p1",
+            "bl": "boq_assistant-bard-web-server_20230514.20_p0",
             "_reqid": str(self._reqid),
             "rt": "c",
         }
