c501d2d6fee6cbdeaf17756a304ec9d2d222000a
Antonio <acheong@student.dalat.org>
2023-06-26 03:45:55 +0800

    1.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="GoogleBard",
-    version="1.3.2",
+    version="1.3.3",
     license="MIT License",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
